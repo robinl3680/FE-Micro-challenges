da6ef36ee4dff3e63591627ee34a7200e4b4bbf5
robin r <robinl3680@gmailc.om>
2025-04-26 19:14:16 +0530

    fix: updated the build

--- otp-input/vite.config.ts
+++ otp-input/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/FE-Micro-challenges/file-explorer/",
+  base: "/FE-Micro-challenges/otp-input/",
   plugins: [react()],
 });
